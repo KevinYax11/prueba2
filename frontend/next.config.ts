dd41f58ef159b7315c98508e26deeac4295dd868
Kevin Miguel Yax Puác <titopuac342@gmail.com>
2026-04-14 00:12:46 -0600

    Add files via upload

---
+++ frontend/next.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  outputFileTracingRoot: __dirname,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
+      },
+    ]
+  },
+}
